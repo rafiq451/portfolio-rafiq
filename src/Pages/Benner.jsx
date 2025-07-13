00f7678dbf9358d46de674e11985117c2fa58155
rafiq451 <ibab7810@gmail.com>
2025-07-13 12:02:58 +0700

    update benner

--- src/Pages/Benner.jsx
+++ src/Pages/Benner.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 import foto from '../assets/blogs/benner.png';
+import foto1 from '../assets/blogs/benner2.png';
 
 const BennerSection = styled.section`
   width: 100%;
   background-color: #0d0d0d;
   display: flex;
+  flex-direction: column; /* Tumpuk vertikal */
   justify-content: center;
   align-items: center;
   padding: 40px 20px;
   box-sizing: border-box;
 `;
 
-const BannerImage = styled.img`
+const BannerWrapper = styled.div`
   width: 100%;
   max-width: 1200px;
+  margin: 10px 0;
+`;
+
+const BannerImage = styled.img`
+  width: 100%;
   height: auto;
   border-radius: 12px;
   box-shadow: 0 8px 20px rgba(0, 0, 0, 0.2);
@@ -28,7 +35,12 @@ const BannerImage = styled.img`
 const Benner = () => {
   return (
     <BennerSection>
-      <BannerImage src={foto} alt="gambar benner" />
+      <BannerWrapper>
+        <BannerImage src={foto} alt="gambar benner" />
+      </BannerWrapper>
+      <BannerWrapper>
+        <BannerImage src={foto1} alt="gambar benner2" />
+      </BannerWrapper>
     </BennerSection>
   );
 };
